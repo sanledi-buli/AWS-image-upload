86be2ba4435ce1e879a0c60fb874edc74368cfdd
sanledi buli <sanledi.buli@gmail.com>
2015-01-26 05:02:48 +0700

    Update code

--- src/main/WebService.java
+++ src/main/WebService.java
@@ -1,14 +1,21 @@
 package main;
 
 import java.io.File;
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-import com.amazonaws.services.s3.transfer.TransferManager;
-import com.amazonaws.services.s3.transfer.Upload;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 
 public class WebService {
-	public static void main (String [] args) throws Exception{
-		
+	private static final String AWS_S3_BUCKET = "";
+	private static final String AWS_SECRET_KEY = "";
+	private static final String AWS_ACCESS_KEY = "";
+	
+	public void uploadImageAWS(File filePath){
+		AWSCredentials awsCredentials = new BasicAWSCredentials(AWS_ACCESS_KEY, AWS_SECRET_KEY);
+		AmazonS3 amazonS3 = new AmazonS3Client(awsCredentials);
+//        amazonS3.createBucket(AWS_S3_BUCKET);
+//        System.out.print("Using S3 bucket " + AWS_S3_BUCKET);
+		amazonS3.putObject(AWS_S3_BUCKET, "image.jpg", filePath);
 	}
-
 }
