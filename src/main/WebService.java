dd8054d81a55a86c768d4dc9b5b0e293398d531c
sanledi buli <sanledi.buli@gmail.com>
2015-01-26 05:01:12 +0700

    Init

---
+++ src/main/WebService.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.io.File;
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.s3.transfer.TransferManager;
+import com.amazonaws.services.s3.transfer.Upload;
+
+public class WebService {
+	public static void main (String [] args) throws Exception{
+		
+	}
+
+}
